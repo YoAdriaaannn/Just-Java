ae95a48d977f1a35f2f020062fe2b9142c42f1ac
fr0stsp1re <info@atomiccomputerservice.com>
2018-03-09 23:49:24 -0800

    Lesson 11 part 17. Modified increment and decrement to check for 1-100 cups of coffee ordered.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         CheckBox orderedChocolateTopping = findViewById(R.id.checkbox_chocolate);
         Boolean hasChocolate = orderedChocolateTopping.isChecked();
 
-
+        // Grab a name from the user
         EditText customerName = findViewById(R.id.edit_name);
         String name = customerName.getText().toString();
 
@@ -59,13 +59,13 @@ public class MainActivity extends AppCompatActivity {
      * Method to increase quantity.
      */
     public void increment(View view) {
-        if (quantity > 100 ) {
+        if (quantity >= 100) {
+       
             Toast.makeText(this, "You can only order between 1 and 100 coffees. Sorry!", Toast.LENGTH_SHORT).show();
-            // resets the quantity back to the range specified
-            displayQuantity(quantity -1);
-        }
-        else {quantity = quantity + 1;
-        displayQuantity(quantity);
+
+        } else {
+            quantity = quantity + 1;
+            displayQuantity(quantity);
         }
     }
 
@@ -73,13 +73,12 @@ public class MainActivity extends AppCompatActivity {
      * Method to decrease quantity.
      */
     public void decrement(View view) {
-        if (quantity < 1) {
+        if (quantity < 2) {
             Toast.makeText(this, "You can only order between 1 and 100 coffees. Sorry!", Toast.LENGTH_SHORT).show();
-            // resets the quantity back to the range specified
-            displayQuantity(quantity + 1);
-        }
-        else {quantity = quantity - 1;
-        displayQuantity(quantity);
+
+        } else {
+            quantity = quantity - 1;
+            displayQuantity(quantity);
         }
     }
 
@@ -100,10 +99,11 @@ public class MainActivity extends AppCompatActivity {
 
     private int calculatePrice(int quantity, boolean whipStatus, boolean chocStatus) {
 
+        // Set up some variables to store the prices of items
         int coffeePrice = 5;
         int whippedCreamPrice = 1;
         int chocolatePrice = 2;
-        int totalPrice;
+
 
         // Check to see if whipped cream or chocolate has been added to order. If so adjust price of coffee.
         if (whipStatus) {
@@ -112,8 +112,8 @@ public class MainActivity extends AppCompatActivity {
         if (chocStatus) {
             coffeePrice = coffeePrice + chocolatePrice;
         }
-        totalPrice = coffeePrice * quantity;
-        return totalPrice;
+        return coffeePrice * quantity;
+
     }
 
     /**
